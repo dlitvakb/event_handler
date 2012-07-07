44cedf9609f64ccd65c8039b8375a0496a241ea6
David Litvak Bruno <david.litvakb@gmail.com>
2012-07-07 12:51:22 -0300

    weakref for dying listeners

--- event_handler/events/events.py
+++ event_handler/events/events.py
@@ -1,27 +1,30 @@
+import weakref
+
+
 class EventListener(object):
-    def __init__(self, registered_signals=None):
-        if registered_signals is None:
-            registered_signals = {}
-        self.__REGISTERED_SIGNALS__ = registered_signals
+    def __init__(self):
+        self.__REGISTERED_SIGNALS__ = {}
 
     def on(self, signal_name, callback):
         self.__REGISTERED_SIGNALS__[signal_name] = callback
 
     def respond(self, signal_name, *args, **kwargs):
-        try:
+        if signal_name in self.__REGISTERED_SIGNALS__:
             self.__REGISTERED_SIGNALS__[signal_name](*args, **kwargs)
-        except KeyError:
-            pass # explicitly silenced
 
 class EventThrower(object):
-    def __init__(self, listeners=None):
-        if not listeners:
-            listeners = []
-        self.__LISTENERS__ = listeners
+    def __init__(self):
+        self.__LISTENERS__ = []
 
     def emit(self, signal_name, *args, **kwargs):
+        dead_listeners = []
         for listener in self.__LISTENERS__:
-            listener.respond(signal_name, *args, **kwargs)
+            if listener() is None:
+                dead_listeners.append(listener)
+                continue
+            listener().respond(signal_name, *args, **kwargs)
+        for dead in dead_listeners:
+            self.__LISTENERS__.remove(dead)
 
     def add_listener(self, listener):
-        self.__LISTENERS__.append(listener)
+        self.__LISTENERS__.append(weakref.ref(listener))
--- tests/events/events.py
+++ tests/events/events.py
@@ -20,3 +20,21 @@ class EventedTest(TestCase):
         thrower.emit('something')
 
         self.assertEqual(listener.foo, 'bar')
+
+    def test_a_listener_can_be_killed(self):
+        listener = SomeListener()
+        listener.on('something', listener.listen_something)
+
+        thrower = EventThrower()
+        thrower.add_listener(listener)
+
+        thrower.emit('something')
+
+        self.assertEqual(listener.foo, 'bar')
+
+        del listener
+        """ Garbage collector is required to simulate time passing """
+        import gc; gc.collect()
+
+        thrower.emit('something')
+        self.assertEqual(0, len(thrower.__LISTENERS__))
